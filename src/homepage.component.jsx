cc2388ef2ce38b2b7f7b2b8fa90e74ecd70be837
Ramana Reddy <ramanareddysane20@gmail.com>
2019-08-17 01:34:36 +0530

    added basic homepage with some styles

---
+++ src/homepage.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './homepage.styles.scss'
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title"> Anjali choudary </h1>
+                    <p className="subtitle">My Sister</p>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title"> Anjali choudary 1 </h1>
+                    <p className="subtitle">My Sister</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+)
+
+export default HomePage;
