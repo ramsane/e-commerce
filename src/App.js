9528dd187985c352f53271b822538750c45c5b4e
Ramana Reddy <ramanareddysane20@gmail.com>
2019-08-18 23:14:00 +0530

    update menu-items to navigate through router

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 
+import {Route, Switch} from 'react-router-dom'
+
+const HatsPage = () => (
+  <h1>Hats Page</h1>
+);
+
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+      <Switch>
+        <Route path='/' component={HomePage} exact />
+        <Route path='/shop/hats' render={HatsPage}/>
+      </Switch>    
   );
 }
 
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import './homepage.styles.scss'
 import Directory from '../../components/directory/directory.component'
 
-const HomePage = () => (
-    <div className='homepage'>
-        <Directory />
+const HomePage = ({history, match}) => {
+    console.log("homepage", match)
+    return (<div className='homepage'>
+        <Directory history={history}/>
     </div>
 
-)
+)}
 
 export default HomePage;
